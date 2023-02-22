af216f523168203187eced0c394de9b66266d753
Jelson J <50907905+jelsonjay@users.noreply.github.com>
2023-02-22 08:59:00 +0000

    Initial Commit

---
+++ LayOutApp/LayOutAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayOutAppApp.swift
+//  LayOutApp
+//
+//  Created by Jelson J on 22/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct LayOutAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
