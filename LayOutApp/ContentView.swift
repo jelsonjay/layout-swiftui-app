3cd92e4d652a05518d2e1d3dc3c1bd41a58dbf1c
Jelson J <50907905+jelsonjay@users.noreply.github.com>
2023-02-22 10:42:44 +0000

    Swiftui layout

--- LayOutApp/ContentView.swift
+++ LayOutApp/ContentView.swift
@@ -8,19 +8,72 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var name = "Swift programming language"
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        ZStack {
+            //Color.mint.opacity(0.5)
+            Rectangle()
+            //.fill(Color("Skyblue").gradient)
+                .fill(
+                    Gradient(colors: [Color("Vermelho"), Color("Amarelo")])
+                )
+                .ignoresSafeArea()
+            GeometryReader { proxy in
+                VStack (spacing: 25){
+                    
+                    Text("JavaScript cool programming language")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(Color("Skyblue"))
+                        .cornerRadius(9)
+                    
+                    
+                    Spacer()
+                    Text(name)
+                        .minimumScaleFactor(0.5)
+                        .multilineTextAlignment(.center)
+                    //.padding()
+                        .frame(height: 120)
+                        .frame(maxWidth: .infinity)
+                        .border(.mint)
+                        .cornerRadius(1)
+                    
+                    Spacer()
+                    Divider()
+                        .background(.black)
+                        .padding()
+                        .frame(width: 170)
+                    Rectangle()
+                        .fill(.indigo)
+                        .frame(width: proxy.size.width * (2/3), height: 1)
+                    
+                    HStack{
+                        Button("Press Me"){
+                            name = "SwiftUI is good framework"
+                        }
+                        .buttonStyle(.borderedProminent)
+                        Spacer()
+                        
+                        Button("Click Button", action: {name = "I need stick with iOS "})
+                            .buttonStyle(.borderedProminent)
+                    }
+                    .padding()
+                    //.tint(Color("Skyblue"))
+                }
+            }
         }
-        .padding()
+        
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
+        
         ContentView()
+        
     }
 }
